98eb78c1baac5e418dc5f8ac553abad4bdc2a768
Gavin Hobbs <gavinhobbs13@gmail.com>
2018-01-31 16:08:22 -0600

    Initial Commit

---
+++ getDemoTH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  getDemoTH
+//
+//  Created by Gavin Hobbs on 1/31/18.
+//  Copyright © 2018 Gavin Hobbs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
